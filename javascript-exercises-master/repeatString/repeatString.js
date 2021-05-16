6aa2dfda57944d898abec7bbbf3e7410ec1fa8f4
Peter McNulty <petemc121@gmail.com>
2021-05-16 14:47:38 +0900

    updated hello world and repeatstring

--- javascript-exercises-master/repeatString/repeatString.js
+++ javascript-exercises-master/repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, repeat) {
+  stringRepeat = "";
 
-}
+  if (repeat < 0) {
+    return "ERROR";
+  } else {
+    for (var i = 0; i < repeat; i++) {
+      stringRepeat += string;
+    }
 
-module.exports = repeatString
+    return stringRepeat;
+  }
+};
+
+module.exports = repeatString;
