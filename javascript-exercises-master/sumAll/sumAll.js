ba743e111d4073e422f49020360a7ee0c49fbc5b
Peter McNulty <petemc121@gmail.com>
2021-05-17 13:16:20 +0900

    completed sumAll

--- javascript-exercises-master/sumAll/sumAll.js
+++ javascript-exercises-master/sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
-}
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
-module.exports = sumAll
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  } else {
+    if (a > b) {
+      for (var i = a; i >= b; i--) {
+        sum += i;
+      }
+    } else if (b > a) {
+      for (var i = a; i <= b; i++) {
+        sum += i;
+      }
+    }
+  }
+
+  return sum;
+};
+
+module.exports = sumAll;
