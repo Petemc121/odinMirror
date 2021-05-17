8e5f205dd115edef1de818e7c81d557c3f7d3d71
Peter McNulty <petemc121@gmail.com>
2021-05-17 14:00:08 +0900

    completed 'leap year' and 'temp conversion' tests

--- javascript-exercises-master/tempConversion/tempConversion.js
+++ javascript-exercises-master/tempConversion/tempConversion.js
@@ -1,12 +1,23 @@
-const ftoc = function() {
+const ftoc = function (f) {
+  let cel = (f - 32) / 1.8;
 
-}
+  if (Number.isInteger(cel)) {
+    return cel;
+  } else {
+    return Math.round(cel * 10) / 10;
+  }
+};
 
-const ctof = function() {
-
-}
+const ctof = function (c) {
+  let far = c * 1.8 + 32;
+  if (Number.isInteger(far)) {
+    return far;
+  } else {
+    return Math.round(far * 10) / 10;
+  }
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
