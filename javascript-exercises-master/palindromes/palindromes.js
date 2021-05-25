e0161e5c1b06516a3deaa9b37ad1d2dd74cff658
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-05-25 14:27:37 +0900

    Update palindromes.js

--- javascript-exercises-master/palindromes/palindromes.js
+++ javascript-exercises-master/palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {};
+const palindromes = function (string) {
+
+    formated = string.replace(/[\W_]+/g,"");
+   lowercase =  formated.toLowerCase();
+
+   reversed = reverse(lowercase);
+
+   if (reversed == lowercase)
+   {
+       return true;
+   } else {
+       return false;
+   }
+
+
+};
+
+function reverse(string) {
+  if(string == "")
+    {
+        return "";
+    }
+
+    return reverse(string.substr(1)) + string.charAt(0);
+}
+
+console.log(palindromes('Hi there, how are you?'));
+
 
 
 module.exports = palindromes
