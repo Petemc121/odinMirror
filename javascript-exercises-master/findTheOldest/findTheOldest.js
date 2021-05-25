68a84cd1155da882f4484c0a197e18686951dea5
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-05-25 15:59:57 +0900

    Update findTheOldest.js

--- javascript-exercises-master/findTheOldest/findTheOldest.js
+++ javascript-exercises-master/findTheOldest/findTheOldest.js
@@ -1,3 +1,36 @@
-let findTheOldest = function() {
+
+
+let findTheOldest = function(people) {
+
+    person = ""
+    oldest = 0;
+
+    for (i = 0; i <people.length; i++)
+    {
+        if ('yearOfDeath' in people[i] == false)
+        {
+        age = 2021 - people[i].yearOfBirth;
+
+        if (age > oldest)
+        {
+            oldest = age;
+            person = people[i];
+        }
+        }
+        else{
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+            if (age > oldest)
+            {
+                oldest = age;
+                person = people[i];
+            }
+            } 
+        
+
+    }
+
+    return person;
+
 }
 module.exports = findTheOldest;
