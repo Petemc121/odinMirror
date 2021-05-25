2b970ad314aee2eec8aec0b3e228279026283599
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-05-25 15:27:39 +0900

    Update fibonacci.js

--- javascript-exercises-master/fibonacci/fibonacci.js
+++ javascript-exercises-master/fibonacci/fibonacci.js
@@ -1,3 +1,44 @@
-const fibonacci = function () {};
+const fibonacci = function (x) {
+    let fibonacciArray = [0,1];
+    if (x < 0)
+    {
+         return 'OOPS';
+
+    } else 
+    if (typeof x == 'string') {
+
+    converted = parseInt(x, 10);
+
+    if (isNaN(converted)) {
+        return "NaN";
+    } else
+
+    {
+    for (i = 2; i < converted +1; i++)
+    {
+        let element = fibonacciArray[i-1] + fibonacciArray[i-2]
+     fibonacciArray.push(element);
+        
+    }
+
+    return fibonacciArray[fibonacciArray.length - 1];
+}
+
+} else {
+    for (i = 2; i < x+1; i++)
+    {
+        let element =fibonacciArray[i-1] + fibonacciArray[i-2]
+     fibonacciArray.push(element);
+        
+    }
+
+    return fibonacciArray[fibonacciArray.length - 1];
+}
+
+}
+   
+
+
+console.log(fibonacci('1'));
 
 module.exports = fibonacci
